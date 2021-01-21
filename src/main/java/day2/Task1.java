b31f7180ffa7294f9e7b3254214d6f56ab8b5664
Natalia Sidorkina <nataly-sd@yandex.ru>
2021-01-21 10:33:58 +0300

    Add Day2 solution

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,20 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        int k = scanner.nextInt();
+        if (k > 0 && k < 5) {
+            System.out.println("Малоэтажный дом");
+        } else if (k > 4 && k < 9) {
+            System.out.println("Среднеэтажный дом");
+        } else  if (k > 8) {
+            System.out.println("Многоэтажный дом");
+        } else if (k <= 0) {
+            System.out.println("Ошибка ввода");
+        }
     }
 }
